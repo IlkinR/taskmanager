12fa5152f80799df3836cc15f6b264fc4198d212
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-20 22:39:45 +0400

    change debug to true for testing purposes

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*^#cz^$&zi__b(v3$auj$ur^7ha2%33@j&6vu%7m6h!_4+2==_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["rbtaskmanager.herokuapp.com", "127.0.0.1"]
 
