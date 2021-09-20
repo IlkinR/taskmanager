ad64dd0d4a8f63663c7ede2238e7828ad71ba138
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-20 22:30:12 +0400

    add dj_bd_url package

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +79,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
