b145e248bfc7427991e53f0953271bf4b68543d6
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-20 21:25:26 +0400

    fix task_delete view

--- taskmanager/todo/urls.py
+++ taskmanager/todo/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("", views.UserLoginView.as_view(), name="login"),
     path("register/", views.UserRegistrationView.as_view(), name="register"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
-    
     # Project
     path("projects/", views.ProjectsListView.as_view(), name="projects"),
     path("project_new/", views.ProjectCreateView.as_view(), name="project_create"),
@@ -18,4 +17,5 @@ urlpatterns = [
     path("project<int:project_id>/tasks", views.TasksListView.as_view(), name="tasks"),
     path("task_new/<int:project_id>/", views.TaskCreateView.as_view(), name="task_create"),
     path("task_update/<int:pk>/", views.TaskUpdateView.as_view(), name="task_update"),
+    path("task_delete/<int:pk>/", views.TaskDeleteView.as_view(), name="task_delete"),
 ]
